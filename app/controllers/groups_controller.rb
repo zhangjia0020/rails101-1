29709c5e8294e2481362c645e2ba9cf399cfddb2
apple <apple@appledeMacBook-Pro.local>
2017-04-11 21:39:51 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,13 @@ end
 
      redirect_to groups_path
  end
+ def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
+  end
 
  private
 
